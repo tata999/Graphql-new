11e0cb7df978d3ad5d359fea3fb86229eb3e1d71
Tata999 <tata.b1821@gmail.com>
2018-07-21 17:35:40 -0700

    Test for PipeLine

--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/datafetcher/AllBooksDataFetcher.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/datafetcher/AllBooksDataFetcher.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Component
 public class AllBooksDataFetcher implements DataFetcher<List<Book>>{
-//Added 
+//Added to look in the pipeline
     @Autowired
     BookRepository bookRepository;
 
