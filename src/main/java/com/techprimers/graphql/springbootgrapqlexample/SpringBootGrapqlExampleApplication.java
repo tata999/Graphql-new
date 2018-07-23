38b0b9f4cc9fef2913d932fb26a1a823e3e39c45
Tata999 <tata.b1821@gmail.com>
2018-07-22 22:54:35 -0700

    Latest Changes

--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/SpringBootGrapqlExampleApplication.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/SpringBootGrapqlExampleApplication.java
@@ -2,11 +2,20 @@ package com.techprimers.graphql.springbootgrapqlexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootGrapqlExampleApplication {
 
-	public static void main(String[] args) {
+public class SpringBootGrapqlExampleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootGrapqlExampleApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootGrapqlExampleApplication.class, args);
 	}
+
 }
